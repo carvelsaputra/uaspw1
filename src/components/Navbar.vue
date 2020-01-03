b3f22b3beb906754c6c3507b2545cee4f1c487f6
Carvel Saputra <carvel.saputra@gmail.com>
2020-01-03 18:12:19 +0700

    Commit navbar, carrousel, parallax
    pertemuan pertama

---
+++ src/components/Navbar.vue
@@ -0,0 +1,24 @@
+<template>
+      <div>
+        <v-app-bar fixed app dark>
+            <v-toolbar-title><span class="headline">Ujian Akhir Semester</span></v-toolbar-title>
+            <v-spacer></v-spacer>
+            <v-toolbar-items>
+                <v-btn text>Home</v-btn>
+                <v-btn text>About</v-btn>
+            </v-toolbar-items>
+        </v-app-bar>
+      </div>
+     
+</template>
+
+<script>
+export default {
+    name: 'Navbar',
+    data: () => ({
+  }),
+};
+</script>
+
+<style>
+</style>
