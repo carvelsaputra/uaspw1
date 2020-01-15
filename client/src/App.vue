6eefe150f96dcb271b67db09afa73d971b7cd688
Jonathan Tanuwijaya <jonathantw64@gmail.com>
2020-01-15 12:04:42 +0700

    Merge remote-tracking branch 'dev/frontend-dev' into dev

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,43 +1,47 @@
 <template>
-    <v-app>
-        <Navbar/>
-        <v-content>
-            <Carousel/>
-            <v-parallax height="300" src="https://cdn.vuetifyjs.com/images/parallax/material.jpg">
-                <v-flex justify-center>
-                    <p class="headline text-center">Developers Team</p>
-                </v-flex>
-            </v-parallax>
-            <v-container class="main-content" fluid gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)">
-                <Content/>
-            </v-container>
-            <Footer/>
-        </v-content>
-    </v-app>
+  <v-app>
+<Navbar/>
+    <v-content>
+       <Carousel/>
+       <v-parallax height="300" src="https://cdn.vuetifyjs.com/images/parallax/material.jpg">
+      <v-flex justify-center>
+                <p class="headline text-center">Developers Team</p>
+      </v-flex>
+       </v-parallax>
+       <v-container class="main-content" fluid gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)">
+        <Content/>
+       </v-container>
+       <Footer/>
+    </v-content>
+  </v-app>
 </template>
 
 <script>
-    import Navbar from './components/Navbar';
-    import Carousel from './components/Carousel';
-    import Content from './components/Content';
-    import Footer from './components/Footer';
-    export default {
-        name: 'App',
-        components:
-            {
-                Navbar,Carousel,Content,Footer
-            },
+import Navbar from './components/Navbar';
+import Carousel from './components/Carousel';
+import Content from './components/Content';
+import Footer from './components/Footer';
+
+export default {
+  name: 'App',
+
+  components:
+  {
+    Navbar,Carousel,Content,Footer
+  },
 
 
-    };
+};
 </script>
 <style scoped>
-    .main-content
-    {
-        background:url('./assets/background/wp2778609-polygonal-wallpapers.jpg');
-        background-repeat: no-repeat;
-        background-size: cover;
-        height: 100%;
-        width:100%;
-    }
+.main-content
+{
+  background:url('./assets/background/wp2778609-polygonal-wallpapers.jpg');
+  background-repeat: no-repeat;
+  background-size: cover;
+   height: 100%;
+   width:100%;
+
+}
+
 </style>
--- client/src/components/Content.vue
+++ client/src/components/Content.vue
@@ -1,132 +1,143 @@
 <template>
-     <v-container  class="content-1 rounded-card">
-           <v-row>
+    <v-container class="content-1 rounded-card">
+        <v-row>
             <v-card
-              width="100%"
-              class="ma-3 pa-6 rounded-card "
-              outlined
-              tile
-              center
-              v-for="(item,i) in items"
-              :key="i"
+                    width="100%"
+                    class="ma-3 pa-6 rounded-card "
+                    outlined
+                    tile
+                    center
+                    v-for="(item,i) in items"
+                    :key="i"
             >
-              <v-col id="tes">
-                <v-row>
-                  <!-- kolom kiri -->
-                  <v-col cols="4">
-                    <v-img height="300" width="200" :src="item.foto" ></v-img>
-                  </v-col>
-                  <!-- kolom kanan -->
-                  <v-col>
+                <v-col id="tes">
                     <v-row>
-                      <v-col cols="2">
-                        <p class="headline">Nama  </p>
-                      </v-col>
-                      <v-col cols="1"><p class="headline">:</p></v-col>
-                      <v-col cols="9">
-                        <p class="headline">{{item.nama}}</p>
-                      </v-col>
-                    </v-row>
-                    <v-row>
-                        <v-col cols="2">
-                          <p class="headline">NPM</p>
+                        <!-- kolom kiri -->
+                        <v-col cols="4">
+                            <v-img height="300" width="200" :src="item.foto"></v-img>
                         </v-col>
-                        <v-col cols="1"><p class="headline">:</p></v-col>
-                        <v-col cols="6">
-                          <p class="headline">{{item.npm}}</p>
+                        <!-- kolom kanan -->
+                        <v-col>
+                            <v-row>
+                                <v-col cols="2">
+                                    <p class="headline">Nama </p>
+                                </v-col>
+                                <v-col cols="1"><p class="headline">:</p></v-col>
+                                <v-col cols="9">
+                                    <p class="headline">{{item.nama}}</p>
+                                </v-col>
+                            </v-row>
+                            <v-row>
+                                <v-col cols="2">
+                                    <p class="headline">NPM</p>
+                                </v-col>
+                                <v-col cols="1"><p class="headline">:</p></v-col>
+                                <v-col cols="6">
+                                    <p class="headline">{{item.npm}}</p>
+                                </v-col>
+                            </v-row>
+                            <p>
+                                {{item.desc}}
+                            </p>
+                            <v-footer>
+                                <v-btn
+                                        v-for="(icon,i) in icons"
+                                        :key="i"
+                                        icon
+                                        class="mx-4"
+                                v-bind:href="items.urlIG">
+                                    <v-img width="24px"
+                                           :src="icon.icon"></v-img>
+                                </v-btn>
+                            </v-footer>
                         </v-col>
                     </v-row>
-                   <p>
-                     {{item.desc}}
-                   </p>
-                   <v-footer>
-                     <v-btn 
-                     v-for="(icon,i) in icons"
-                     :key="i"
-                     icon
-                     class="mx-4">
-                      <v-img width="24px" 
-                      :src="icon.icon"></v-img>
-                     </v-btn>
-                   </v-footer>
-                  </v-col>
-                </v-row>
-              </v-col>
+                </v-col>
             </v-card>
-           </v-row>
-           
-     </v-container>
+        </v-row>
+
+    </v-container>
 </template>
 <script>
+    import axios from 'axios'
 
-export default {
+    export default {
+        name: 'Content',
+        data: () => ({
+            items: [
+                {
+                    nama: "Carvel Saputra Martaloho",
+                    npm: "1822250008",
+                    foto: require("../../../server/data/profile/bagas.jpeg"),
+                    desc: "Halo, saya Carvel Saputra Martaloho prodi TI angkatan 2018.. saya lahir di Palembang, SD di Xaverius 4, SMP Xaverius Maria, dan SMA di Xaverius 3"
+                },
+                {
+                    nama: "Jonathan Tanuwijaya",
+                    npm: "1822250010",
+                    foto: require("../assets/logo.png")
+                },
+                {
+                    nama: "Edward Agustria",
+                    npm: "1822250010",
+                    foto: require("../assets/profile/titi.jpeg")
+                },
+                {
+                    nama: "Ricky Yohannes",
+                    npm: "1822250010",
+                    foto: require("../assets/profile/ricky.jpeg")
+                },
+                {
+                    nama: "Richardo Anggara",
+                    npm: "1822250010",
+                    foto: require("../assets/profile/dodo.jpeg")
+                },
+                {
+                    nama: "Refaldy Bagas Riyanto",
+                    npm: "1822250010",
+                    foto: require("../assets/profile/bagas.jpeg")
+                }
+            ],
 
-    name:'Content',
-    data:()=>({
-        items:[
-            {
-              nama:"Carvel Saputra Martaloho",
-              npm:"1822250008",
-              foto:require("../assets/profile/CarvelSaputraMartaLoho.jpg"),
-              desc:"Halo, saya Carvel Saputra Martaloho prodi TI angkatan 2018.. saya lahir di Palembang, SD di Xaverius 4, SMP Xaverius Maria, dan SMA di Xaverius 3"
-            },
-            {
-              nama:"Jonathan Tanuwijaya",
-              npm:"1822250010",
-              foto:require("../assets/logo.png")
-            },
-             {
-              nama:"Edward Agustria",
-              npm:"1822250010",
-              foto:require("../assets/profile/titi.jpeg")
-            },
-            {
-              nama:"Ricky Yohannes",
-              npm:"1822250010",
-              foto:require("../assets/profile/ricky.jpeg")
-            },
-            {
-              nama:"Richardo Anggara",
-              npm:"1822250010",
-              foto:require("../assets/profile/dodo.jpeg")
-            },
-            {
-              nama:"Refaldy Bagas Riyanto",
-              npm:"1822250010",
-              foto:require("../assets/profile/bagas.jpeg")
-            }
+            icons:
+                [
+                    {
+                        icon: require("../assets/sosmed/instagram.png")
+                    },
+                    {
+                        icon: require("../assets/sosmed/facebook.png")
+                    }
+                ]
 
-        ],
-        icons:
-        [
-          {
-            icon:require("../assets/sosmed/instagram.png")
-          },
-          {
-            icon:require("../assets/sosmed/facebook.png")
-          }
 
-        ]
-          
-        
+        }),
+        created() {
+            axios
+                .get("http://localhost:8081/")
+                .then(res => {
+                        // this.items = res.data;
+                        console.log(res.data)
+                    }
+                ).catch(error => {
+                console.log(error)
+            })
 
-    })
-}
+        }
+    }
 </script>
-<style  scoped>
-*{
-  color:white;
-}
-.content-1
-{
-  background-color: purple
-}
-.rounded-card{
-    border-radius:10px;
-}
-#tes{
-  background-color: #1976D2
-}
+<style scoped>
+    * {
+        color: white;
+    }
 
+    .content-1 {
+        background-color: purple
+    }
 
+    .rounded-card {
+        border-radius: 10px;
+    }
+
+    #tes {
+        background-color: #1976D2
+    }
 </style>
