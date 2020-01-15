fd166ecb6c6eace73cf42f1406c727173a74a224
Jonathan Tanuwijaya <jonathantw64@gmail.com>
2020-01-15 19:36:48 +0700

    success retrieve all data from server

--- server/server.js
+++ server/server.js
@@ -4,6 +4,25 @@ const router = require('./route/route');
 const cors = require('cors');
 const app = express();
 
+
+// app.get('/profile/bagas.jpeg',(req,res)=>{
+//
+//
+//
+//     res.send({
+//         test:'oke'
+//     })
+// })
+//
+// // Ini Middleware
+// app.use((req,res,next)=>{
+//
+//
+//     console.log(req.url)
+//     next() // ini buat trigger block ny
+// })
+
+app.use(express.static('data'));
 app.use(cors());
 app.use(router);
 app.use(bodyParser.json());
